362e333d184926e03680d9326e62720268187684
Brend Wanders <b.wanders@utwente.nl>
2013-04-04 09:56:45 +0200

    Adds editting for pages.

--- tvarita/__init__.py
+++ tvarita/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, url_for, render_template, redirect, abort
+from flask import Flask, url_for, render_template, redirect, abort, request
 import os
 import markdown
 
@@ -18,6 +18,14 @@ def get_page_source(page):
         requested_page = os.path.join(config.page_path, normalize(page))
         with open(requested_page) as f:
             return f.read()
+    else:
+        return ''
+
+
+def save_page(page, source):
+    saved_page = os.path.join(config.page_path, normalize(page))
+    with open(saved_page, 'w') as f:
+        f.write(source)
 
 
 def page_exists(page):
@@ -25,12 +33,16 @@ def page_exists(page):
     return os.path.isfile(requested_page)
 
 
+def render_page(page):
+    page_source = get_page_source(page)
+    return markdown.markdown(page_source, safe_mode='escape', output_format='html5', extensions=['wikilinks'])
+
 @app.route('/')
 def index():
     return redirect(url_for('serve_page', page=normalize('start')))
 
 
-@app.route('/<page>')
+@app.route('/<page>/')
 def serve_page(page):
     if normalize(page) != page:
         return redirect(url_for('serve_page', page=normalize(page)))
@@ -38,13 +50,31 @@ def serve_page(page):
     if not page_exists(page):
         abort(404)
 
+    result = render_page(page)
+    return render_template('page.html', content = result, title = page, page=page)
+
+@app.route('/<page>/edit/')
+def edit_page(page):
+    if normalize(page) != page:
+        return redirect(url_for('edit_page',page=normalize(page)))
+
     page_source = get_page_source(page)
-    result = markdown.markdown(page_source, safe_mode='escape', output_format='html5')
-    return render_template('page.html', content = result, title = page)
 
+    return render_template('edit.html', content=page_source, title = page + ' [edit]', page=page)
 
+@app.route('/<page>/edit/', methods=['POST'])
+def edit_page_posted(page):
+    if normalize(page) != page:
+        abort(405)
+
+    new_page_source = request.form['page']
+
+    save_page(page, new_page_source)
+
+    return redirect(url_for('serve_page', page=page))
 
 @app.errorhandler(404)
+@app.errorhandler(405)
 def page_not_found(error):
     return render_template('error.html', error=error), 404
 
